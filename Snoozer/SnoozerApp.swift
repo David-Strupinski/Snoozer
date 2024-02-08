97e98a5dfaab91574cba22363595a379ee84bb60
David Strupinski <david.strupinski@gmail.com>
2024-02-08 12:31:56 -0800

    Initial Commit

---
+++ Snoozer/SnoozerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozerApp.swift
+//  Snoozer
+//
+//  Created by David Strupinski on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SnoozerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
