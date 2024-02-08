59b2b133f7b705ee142f234ef83243d119cd2189
David Strupinski <david.strupinski@gmail.com>
2024-02-08 15:41:33 -0800

    start homescreen UI

--- Snoozer/ContentView.swift
+++ Snoozer/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import UIKit
+import CoreHaptics
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("7:00")
+                .font(.system(size: 100))
+            
+            Image(systemName: "alarm")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            Button(action: {
+                print("You Snooze You Lose")
+            }) {
+                Text("Snoozer")
+                    .font(.system(size: 40))
+                    .padding(.horizontal, 50)
+                    .background(Color.orange)
+                    .foregroundColor(.black)
+                    .clipShape(Capsule())
+            }
+            .padding(50)
+            
+            Spacer()
+            
+            GroupBox(label: Label("Home", systemImage: "plus")) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+            }
         }
-        .padding()
     }
 }
 
