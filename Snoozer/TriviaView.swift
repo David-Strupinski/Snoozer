fa25d0343ff49f0211e57c53e0508842fbfd78f5
Betty-Cheng <wishfulbetty@gmail.com>
2024-02-14 15:03:57 -0800

    TriviaView Swift file

---
+++ Snoozer/TriviaView.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaView.swift
+//  Snoozer
+//
+//  Created by Amy Cheng on 2/14/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+// list of questions
+// how to connect database for different questions
