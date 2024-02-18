191f757d9d120f052b41d774b04dd51fcfef631f
David Strupinski <david.strupinski@gmail.com>
2024-02-17 19:00:18 -0800

    store alarms in state

--- Snoozer/AlarmView.swift
+++ Snoozer/AlarmView.swift
@@ -9,22 +9,44 @@ import Foundation
 import SwiftUI
 
 struct AlarmView: View {
-    @State private var alarmOn = false
-    
+    // TODO: make this more global to hold state past this page
+    @State private var alarms: [Date: Bool] = [
+        stringToTime(str: "7:00 AM"): true,
+        stringToTime(str: "8:00 AM"): false,
+        stringToTime(str: "10:00 PM"): true
+    ]
     
     var body: some View {
         VStack {
             Text("Alarm")
             
             // repeat per alarm added
-            HStack {
-                Toggle(isOn: $alarmOn) {
-                    Text("7:00 AM")
+            ForEach(alarms.sorted(by: { $0.key < $1.key }), id: \.key) { (time, active) in
+                HStack {
+                    Toggle(isOn: Binding(
+                        get: { alarms[time] ?? false },
+                        set: { alarms[time] = $0 }
+                    )) {
+                        Text(timeToString(time: time))
+                    }
+                    .toggleStyle(.switch)
                 }
-                .toggleStyle(.switch)
+                .padding()
             }
         }
     }
+}
+
+func stringToTime(str: String) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "hh:mm a"
+    return dateFormatter.date(from: str)!
+}
+
+func timeToString(time: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "hh:mm a"
+    return dateFormatter.string(from: time)
 }
 
 #Preview {
