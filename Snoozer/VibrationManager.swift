2d4fcda18c1390b8bb2d2438bcbba04a07e746d2
Nathan <natesdk@cs.washington.edu>
2024-02-25 09:23:57 -0800

    made edits on soundmanager

--- Snoozer/VibrationManager.swift
+++ Snoozer/VibrationManager.swift
@@ -5,30 +5,29 @@
 //  Created by Nathan Ryan Sidik on 2/20/24.
 //
 
-import AudioToolbox
+import SwiftUI
+import AVKit
 
-class VibrationManager {
+class SoundManager {
+    static let instance = SoundManager() // singleton
     
-    static let shared = VibrationManager() // Singleton instance
+    var player: AVAudioPlayer?
     
-    private init() {} // Private initializer to ensure singleton usage
-    
-    func vibrate() {
-        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-    }
-    
-    // Custom pattern vibration (iOS 13 and later)
-    // duration: time in seconds; intensity: 0 (min) to 1 (max)
-    @available(iOS 13.0, *)
-    func vibrateWithPattern(duration: Double, intensity: Float) {
-        let vibrationPattern: [NSNumber] = [0, NSNumber(value: duration)]
-        let intensityPattern: [NSNumber] = [NSNumber(value: intensity)]
+    func playSound() {
+        guard let url = URL(string: "") else { return }
         
-//        var dictionary = [String: Any]()
-//        dictionary[kCustomVibrationPatternKey] = vibrationPattern
-//        dictionary[kVibrationIntensityKey] = intensityPattern
-//        
-//        AudioServicesStopSystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-//        AudioServicesPlaySystemSoundWithVibration(SystemSoundID(kSystemSoundID_Vibrate), nil, dictionary)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+        
+        
     }
 }
+//
+//struct VibrationManager: View {
+//    var soundManager = SoundManager()
+//    
+//}
