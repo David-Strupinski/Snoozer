f06a241e58a3c6ed7576f513c08cb44856b2a9b2
Amy Cheng <chengamy5@gmail.com>
2024-03-11 20:43:14 -0700

    Add Chat UI files

---
+++ Snoozer/ChatUIView.swift
@@ -0,0 +1,57 @@
+//
+//  ChatUIView.swift
+//  Snoozer
+//
+//  Created by Amy Cheng on 3/11/24.
+//
+
+import SwiftUI
+import StreamChat
+import StreamChatSwiftUI
+
+struct ChatUIView: App {
+    var chatClient: ChatClient = {
+            var config = ChatClientConfig(apiKey: .init("8br4watad788"))
+            config.isLocalStorageEnabled = true
+            config.applicationGroupIdentifier = "group.io.getstream.iOS.ChatDemoAppSwiftUI"
+
+            // The resulting config is passed into a new `ChatClient` instance.
+            let client = ChatClient(config: config)
+            return client
+        }()
+        
+        @State var streamChat: StreamChat?
+        
+        init() {
+            streamChat = StreamChat(chatClient: chatClient)
+            connectUser()
+        }
+        
+        var body: some Scene {
+            WindowGroup {
+//                ChatChannelListView()
+                CustomChannelList()
+            }
+        }
+        
+        private func connectUser() {
+            // This is a hardcoded token valid on Stream's tutorial environment.
+            let token = try! Token(rawValue: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVrZV9za3l3YWxrZXIifQ.kFSLHRB5X62t0Zlc7nwczWUfsQMwfkpylC6jCUZ6Mc0")
+            
+            // Call `connectUser` on our SDK to get started.
+            chatClient.connectUser(
+                userInfo: .init(
+                    id: "luke_skywalker",
+                    name: "Luke Skywalker",
+                    imageURL: URL(string: "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg")!
+                ),
+                token: token
+            ) { error in
+                if let error = error {
+                    // Some very basic error handling only logging the error.
+                    log.error("connecting the user failed \(error)")
+                    return
+                }
+            }
+        }
+}
